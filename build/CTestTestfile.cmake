71a551b1298e8a9d0622dacd9c43f24cef3c575f
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2019-08-29 19:00:33 +0000

    changed file structure

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/src
+# Build directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(catkin_simple)
+subdirs(bill_teleop_joy)
+subdirs(cartbot_motor_drivers)
---
+++ build/cartbot_motor_drivers/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/src/cartbot_motor_drivers
+# Build directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/build/cartbot_motor_drivers
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

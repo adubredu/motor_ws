30478a6ddb51b1421fc9c0a080af539e9afaed06
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2019-08-29 19:04:20 +0000

    removed binaries

--- build/bill_teleop_joy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/src/bill_teleop_joy
-# Build directory: /home/ubuntu/cartbot_mobilebase_subsystem/cartbot_ws/build/bill_teleop_joy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
